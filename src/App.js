2518dba89fc404eafc82cdf8035a41a3dc996639
Nguyen Huyen <huyencapuchino2512@gmail.com>
2021-12-27 17:13:36 +0700

    Update App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is my first ReactJS app, Please don't ...</h1>  
+    </div>
+  );
+}
+
+export default App;
